dbcf354b240b9db6744cc1dac6adb3436ebd79a7
Simon Horton <sij.horton@gmail.com>
2012-11-07 00:43:43 +0100

    reverted to 0.0.19 and everything works again.

--- data/app.js
+++ data/app.js
@@ -28,76 +28,9 @@ if (process.argv.length>2) {
 		appPackage.launch(pInfo);
 	});
 } else {
+	var app = module.exports = require('appjs');
+
 	app.serveFilesFrom(__dirname + '/content');
-
-	var menubar = app.createMenu([{
-	  label:'&File',
-	  submenu:[
-		{
-		  label:'E&xit',
-		  action: function(){
-			window.close();
-		  }
-		}
-	  ]
-	},{
-	  label:'&Window',
-	  submenu:[
-		{
-		  label:'Fullscreen',
-		  action:function(item) {
-			window.frame.fullscreen();
-			console.log(item.label+" called.");
-		  }
-		},
-		{
-		  label:'Minimize',
-		  action:function(){
-			window.frame.minimize();
-		  }
-		},
-		{
-		  label:'Maximize',
-		  action:function(){
-			window.frame.maximize();
-		  }
-		},{
-		  label:''//separator
-		},{
-		  label:'Restore',
-		  action:function(){
-			window.frame.restore();
-		  }
-		}
-	  ]
-	}]);
-
-	menubar.on('select',function(item){
-	  console.log("menu item "+item.label+" clicked");
-	});
-
-	var trayMenu = app.createMenu([{
-	  label:'Show',
-	  action:function(){
-		window.frame.show();
-	  },
-	},{
-	  label:'Minimize',
-	  action:function(){
-		window.frame.hide();
-	  }
-	},{
-	  label:'Exit',
-	  action:function(){
-		window.close();
-	  }
-	}]);
-
-	var statusIcon = app.createStatusIcon({
-	  icon:'./data/content/icons/32.png',
-	  tooltip:'AppJS Hello World',
-	  menu:trayMenu
-	});
 
 	var window = app.createWindow({
 	  width  : 640,
@@ -109,11 +42,11 @@ if (process.argv.length>2) {
 	  console.log("Window Created");
 	  window.frame.show();
 	  window.frame.center();
-	  window.frame.setMenuBar(menubar);
 	});
 
 	window.on('ready', function(){
 	  console.log("Window Ready");
+	  window.require = require;
 	  window.process = process;
 	  window.module = module;
 
@@ -125,6 +58,10 @@ if (process.argv.length>2) {
 		  window.frame.openDevTools();
 		}
 	  });
+	});
+
+	window.on('close', function(){
+	  console.log("Window Closed");
 	});
 
 }
